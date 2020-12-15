60f1ca8304fefedaea6ec2ff551cc7ffcd6f9b9c
Jagger88 <31702861+Jagger88@users.noreply.github.com>
2020-12-14 23:57:50 -0500

    Initial

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Bryant's Update
         </a>
       </header>
     </div>
